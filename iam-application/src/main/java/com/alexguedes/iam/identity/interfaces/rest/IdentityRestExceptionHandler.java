f5e4273266bc7f457a0c3be361d12a18556e8080
AlexGuedesSilva <alex__guedes@hotmail.com>
2026-06-29 19:13:31 -0300

    feat(identity): add global exception handler

---
+++ iam-application/src/main/java/com/alexguedes/iam/identity/interfaces/rest/IdentityRestExceptionHandler.java
@@ -0,0 +1,83 @@
+package com.alexguedes.iam.identity.interfaces.rest;
+
+import com.alexguedes.iam.identity.domain.exception.InvalidEmailException;
+import com.alexguedes.iam.identity.domain.exception.InvalidPasswordException;
+import com.alexguedes.iam.identity.domain.exception.InvalidUserNameException;
+import com.alexguedes.iam.identity.domain.exception.UserAlreadyExistsException;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class IdentityRestExceptionHandler {
+
+    private static final String INTERNAL_SERVER_ERROR_MESSAGE = "Internal server error";
+
+    @ExceptionHandler(UserAlreadyExistsException.class)
+    public ResponseEntity<ErrorResponse> handleUserAlreadyExists(
+            UserAlreadyExistsException exception,
+            HttpServletRequest request
+    ) {
+        return buildResponse(HttpStatus.CONFLICT, exception.getMessage(), request);
+    }
+
+    @ExceptionHandler({
+            InvalidEmailException.class,
+            InvalidPasswordException.class,
+            InvalidUserNameException.class
+    })
+    public ResponseEntity<ErrorResponse> handleInvalidDomainInput(
+            RuntimeException exception,
+            HttpServletRequest request
+    ) {
+        return buildResponse(HttpStatus.BAD_REQUEST, exception.getMessage(), request);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception,
+            HttpServletRequest request
+    ) {
+        return buildResponse(HttpStatus.BAD_REQUEST, validationMessage(exception), request);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGenericException(
+            Exception exception,
+            HttpServletRequest request
+    ) {
+        return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR, INTERNAL_SERVER_ERROR_MESSAGE, request);
+    }
+
+    private ResponseEntity<ErrorResponse> buildResponse(
+            HttpStatus status,
+            String message,
+            HttpServletRequest request
+    ) {
+        return ResponseEntity
+                .status(status)
+                .body(new ErrorResponse(
+                        status.value(),
+                        status.getReasonPhrase(),
+                        message,
+                        request.getRequestURI(),
+                        Instant.now()
+                ));
+    }
+
+    private String validationMessage(MethodArgumentNotValidException exception) {
+        return exception.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .findFirst()
+                .map(FieldError::getDefaultMessage)
+                .orElse("Request body is invalid");
+    }
+}

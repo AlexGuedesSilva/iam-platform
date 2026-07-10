901097d78fe2eed6147e3e4716440ef0915cdad4
AlexGuedesSilva <alex__guedes@hotmail.com>
2026-07-10 19:32:16 -0300

    fix(token): configure RS256 key properties

--- iam-application/src/main/java/com/alexguedes/iam/identity/infrastructure/security/token/Rs256JwtProperties.java
+++ iam-application/src/main/java/com/alexguedes/iam/identity/infrastructure/security/token/Rs256JwtProperties.java
@@ -3,5 +3,9 @@ package com.alexguedes.iam.identity.infrastructure.security.token;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "iam.security.jwt.rs256")
-public record Rs256JwtProperties(String privateKey, String publicKey, String keyId) {
+public record Rs256JwtProperties(
+        String privateKey,
+        String publicKey,
+        String keyId
+) {
 }
